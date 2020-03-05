33af44376bb35064c421ef5fcf8c62fc5617891d
Ashwin <ashwinr2k2@gmail.com>
2020-03-05 20:52:39 +1100

    elevator subsystem

---
+++ 2020/alpha/src/main/include/subsystems/ElevatorSubsystem.h
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <frc/PWMVictorSPX.h>
+#include "Constants.h"
+
+class ElevatorSubsystem : public frc2::SubsystemBase {
+ public:
+  ElevatorSubsystem();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+  void SetSpeed(double speed);
+ 
+
+ private:
+    frc::PWMVictorSPX m_elevator{ELEVATOR_PORT};
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+};
